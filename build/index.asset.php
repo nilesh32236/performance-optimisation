23f62c22acd60a955e96c34f8869f5136017e483
nilesh-32236 <nilesh.kanzariya@qrolic.com>
2026-04-14 16:29:55 +0530

    build: update asset dependency version for performance-optimisation plugin

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-element'), 'version' => '37eddde256e3c7a39650');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-element'), 'version' => '1b7f9338ca05327bdd3e');
