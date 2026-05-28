77d5786bceaf8d4c3d0f388f9cefe3403d8ad139
nilesh-32236 <nilesh.kanzariya@qrolic.com>
2026-05-28 20:10:52 +0530

    Apply safe npm audit fix (42 vulnerabilities resolved, 8 remaining in dev deps requiring major upgrade)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '09c89010b86499c55b68');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'df67a62276fb63d556ed');
