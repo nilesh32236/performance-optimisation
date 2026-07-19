7b6b0648596c18d77186e0ed0220c1b1968f64d1
nilesh32236 <nilesh32236@users.noreply.github.com>
2026-07-19 14:20:32 +0000

    fix: autofix iteration 3

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b076aab25e049c6d3fd7');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c9737f526b8c79343130');
