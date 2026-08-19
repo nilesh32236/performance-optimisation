4e065e837826e02da8cdeb7840114e088305b7a8
nilesh-32236 <nilesh.kanzariya@qrolic.com>
2026-08-19 18:24:57 +0530

    chore: rebuild after phpcbf

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd5069906cf9b37866837');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '860741b95ce875a104a3');
