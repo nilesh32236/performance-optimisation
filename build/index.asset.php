0f0749ca233c157855a95d0ebf6f683453a1b62e
nilesh32236 <nilesh32236@gmail.com>
2026-09-16 09:25:37 +0000

    chore: rebuild assets from branch sources

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5e431cbcd97398094c14');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '98a4282d0c1ee9b44bad');
