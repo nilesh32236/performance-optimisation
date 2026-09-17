7372575dcb9edcfce739ae135d0362d9e666e14d
nilesh32236 <nilesh32236@gmail.com>
2026-09-17 11:53:32 +0000

    Rebuild assets for Delay-JS presets on current master

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c0fc70ef851408d81b03');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4c2825f5076e1b68074b');
