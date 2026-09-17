42d9e6c7f8959a019f3523246f1aabbb90990efa
nileshkanzariya <116022087+nilesh32236@users.noreply.github.com>
2026-09-17 21:03:49 +0530

    fix(security): tier ESI nonce-refresh budget on receipt proof (from #1332) (#1334)

--- build/esi.asset.php
+++ build/esi.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => '93d48ca43d03af2e2d9e');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '7c467c0608a04a871e4b');
