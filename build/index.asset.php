3a333f3241a456b53f4a3fc7b74a039c16624797
nilesh32236 <nilesh32236@gmail.com>
2026-09-17 09:40:20 +0000

    🎨 Palette: link Load Activity Log button to description (aria-describedby)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '94c76a424b027500eeae');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5f293aac36af6f4d88de');
