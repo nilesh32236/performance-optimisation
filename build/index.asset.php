9720c545d4a069eb4f5948d7635598eca8e7bf43
nilesh32236 <nilesh32236@gmail.com>
2026-09-17 09:04:07 +0000

    chore: rebuild assets after rebase onto post-#1281 master

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3b04f8c8b36d19d8a55c');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd74bb1625f6133e3fe04');
