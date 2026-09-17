7c850e56e6c075e508068948b0430a1bb57d5d92
nilesh32236 <nilesh32236@gmail.com>
2026-09-17 09:35:49 +0000

    build: regenerate assets for woo self-test button refactor

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c8b5c5704ae6d5988ec4');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '94c76a424b027500eeae');
